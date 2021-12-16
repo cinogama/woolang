998ca00653fe18140e4df691c00e03d950823e97
悖论先生 <583903928@qq.com>
2021-12-16 08:03:45 +0800

    update compile scripts

--- build_g++.sh
+++ build_g++.sh
@@ -1,2 +1,9 @@
-g++ RestorableScene/*.cpp -O0 -std=c++17 -lpthread -ldl -shared -fPIC -o librscene.so
-g++ RSceneDriver/*.cpp -O0 -std=c++17 -lrscene -L./ -Wl,-rpath=./ -o rscene
+#!/bin/bash
+enca -L zh_CN -x utf-8 *.hpp
+enca -L zh_CN -x utf-8 *.cpp
+enca -L zh_CN -x utf-8 *.h
+enca -L zh_CN -x utf-8 *.c
+g++ RestorableScene/*.cpp -O0 -std=c++17 -lpthread -ldl -shared -fPIC -o librscene.so -m64
+g++ RSceneDriver/*.cpp -O0 -std=c++17 -lrscene -L./ -Wl,-rpath=./ -o rscene -m64
+g++ RestorableScene/*.cpp -O0 -std=c++17 -lpthread -ldl -shared -fPIC -o librscene32.so -m32
+g++ RSceneDriver/*.cpp -O0 -std=c++17 -lrscene -L./ -Wl,-rpath=./ -o rscene32 -m32
