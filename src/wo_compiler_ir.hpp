74c96e76921b8d30691b8465ec58b619c63ca627
mr_cino <583903928@qq.com>
2022-07-28 14:01:34 +0800

    *minor fix

--- src/wo_compiler_ir.hpp
+++ src/wo_compiler_ir.hpp
@@ -819,18 +819,6 @@ namespace wo
 
             WO_PUT_IR_TO_BUFFER(instruct::opcode::nequs, WO_OPNUM(op1), WO_OPNUM(op2));
         }
-        template<typename OP1T, typename OP2T>
-        void modx(const OP1T& op1, const OP2T& op2)
-        {
-            static_assert(std::is_base_of<opnum::opnumbase, OP1T>::value
-                && std::is_base_of<opnum::opnumbase, OP2T>::value,
-                "Argument(s) should be opnum.");
-
-            static_assert(!std::is_base_of<opnum::immbase, OP1T>::value,
-                "Can not mod value to immediate.");
-
-            WO_PUT_IR_TO_BUFFER(instruct::opcode::modx, WO_OPNUM(op1), WO_OPNUM(op2));
-        }
 
         template<typename OP1T, typename OP2T>
         void addr(const OP1T& op1, const OP2T& op2)
