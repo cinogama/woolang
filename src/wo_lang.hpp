661b3ae7cba91b8fc1e1e79c6ac8eb1477a16ac0
mr_cino <583903928@qq.com>
2022-11-09 18:29:50 +0800

    * TMP: using_sid_op_for_store_value

--- src/wo_lang.hpp
+++ src/wo_lang.hpp
@@ -2165,13 +2165,10 @@ namespace wo
             }
             else if (auto* a_value_assign = dynamic_cast<ast_value_assign*>(value))
             {
-                bool using_sidmap_to_store_value = false;
-                auto* a_value_index_map = dynamic_cast<ast_value_index*>(a_value_assign->left);
-                if (a_value_index_map
-                    && a_value_index_map->from->value_type->is_map()
-                    && a_value_assign->operate == +lex_type::l_assign)
+                bool using_sid_op_for_store_value = false;
+                if (dynamic_cast<ast_value_index*>(a_value_assign->left))
                     // a[xx] = val; generate 'sidmap' opcode!
-                    a_value_index_map->using_sidmap_to_store_value = using_sidmap_to_store_value = true;
+                    using_sid_op_for_store_value = true;
 
                 // if mixed type, do opx
                 bool same_type = a_value_assign->left->value_type->accept_type(a_value_assign->right->value_type, false);
@@ -2186,8 +2183,8 @@ namespace wo
                 }
                 size_t revert_pos = compiler->get_now_ip();
 
-                auto* beoped_left_opnum_ptr = &analyze_value(a_value_assign->left, compiler);
                 auto* op_right_opnum_ptr = &analyze_value(a_value_assign->right, compiler);
+                auto* beoped_left_opnum_ptr = &analyze_value(a_value_assign->left, compiler);
 
                 if (is_cr_reg(*beoped_left_opnum_ptr)
                     //      FUNC CALL                           A + B ...                       A[X] (.E.G)
@@ -2862,10 +2859,7 @@ namespace wo
                         compiler->idarr(beoped_left_opnum, op_right_opnum);
                     else if (a_value_index->from->value_type->is_dict() || a_value_index->from->value_type->is_map())
                     {
-                        if (a_value_index->using_sidmap_to_store_value)
-                            compiler->sidmap(beoped_left_opnum, op_right_opnum);
-                        else
-                            compiler->iddict(beoped_left_opnum, op_right_opnum);
+                        compiler->iddict(beoped_left_opnum, op_right_opnum);
                     }
                     else if (a_value_index->from->value_type->is_string())
                         compiler->idstr(beoped_left_opnum, op_right_opnum);
@@ -3613,11 +3607,11 @@ namespace wo
                         {
                             // Issue N221109: Reference will not support.
                             // All arguments will 'psh' to stack & no 'pshr' command in future.
-                                funcdef->this_func_scope->
-                                    reduce_function_used_stack_size_at(a_value_arg_define->symbol->stackvalue_index_in_funcs);
+                            funcdef->this_func_scope->
+                                reduce_function_used_stack_size_at(a_value_arg_define->symbol->stackvalue_index_in_funcs);
 
-                                wo_assert(0 == a_value_arg_define->symbol->stackvalue_index_in_funcs);
-                                a_value_arg_define->symbol->stackvalue_index_in_funcs = -2 - arg_count - (wo_integer_t)funcdef->capture_variables.size();
+                            wo_assert(0 == a_value_arg_define->symbol->stackvalue_index_in_funcs);
+                            a_value_arg_define->symbol->stackvalue_index_in_funcs = -2 - arg_count - (wo_integer_t)funcdef->capture_variables.size();
                         }
                         else//variadic
                             break;
