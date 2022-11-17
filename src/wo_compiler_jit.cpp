4e39ed4edf352faa1ea59d03e26d074f87c6a70e
mr_cino <583903928@qq.com>
2022-11-17 13:45:54 +0800

    * 处于性能考虑暂时移除所有检查点

--- src/wo_compiler_jit.cpp
+++ src/wo_compiler_jit.cpp
@@ -285,8 +285,11 @@ namespace wo
             }
             return 0;
         }
-        static void make_checkpoint(asmjit::X86Compiler& x86compiler, asmjit::X86Gp rtvm, asmjit::X86Gp stack_sp, asmjit::X86Gp stack_bp, const byte_t* ip)
+        static void make_checkpoint(asmjit::X86Compiler& x86compiler, asmjit::X86Gp rtvm, asmjit::X86Gp stack_sp, asmjit::X86Gp stack_bp, const byte_t* ip, bool force)
         {
+            if (!force)
+                return;
+
             // TODO: OPTIMIZE!
             auto no_interrupt_label = x86compiler.newLabel();
 
@@ -965,7 +968,7 @@ namespace wo
                     if (auto fnd = x86_label_table.find(jmp_place);
                         fnd != x86_label_table.end())
                     {
-                        make_checkpoint(x86compiler, _vmbase, _vmssp, _vmsbp, rt_ip);
+                        make_checkpoint(x86compiler, _vmbase, _vmssp, _vmsbp, rt_ip, false);
                         wo_asure(!x86compiler.jmp(fnd->second));
                     }
                     else
@@ -985,7 +988,7 @@ namespace wo
                     if (auto fnd = x86_label_table.find(jmp_place);
                         fnd != x86_label_table.end())
                     {
-                        make_checkpoint(x86compiler, _vmbase, _vmssp, _vmsbp, rt_ip);
+                        make_checkpoint(x86compiler, _vmbase, _vmssp, _vmsbp, rt_ip, false);
                         wo_asure(!x86compiler.je(fnd->second));
                     }
                     else
@@ -1005,7 +1008,7 @@ namespace wo
                     if (auto fnd = x86_label_table.find(jmp_place);
                         fnd != x86_label_table.end())
                     {
-                        make_checkpoint(x86compiler, _vmbase, _vmssp, _vmsbp, rt_ip);
+                        make_checkpoint(x86compiler, _vmbase, _vmssp, _vmsbp, rt_ip, false);
                         wo_asure(!x86compiler.jne(fnd->second));
                     }
                     else
@@ -1049,7 +1052,7 @@ namespace wo
                     }
 
                     // ATTENTION: AFTER CALLING VM FUNCTION, DONOT MODIFY SP/BP/IP CONTEXT, HERE MAY HAPPEND/PASS BREAK INFO!!!
-                    make_checkpoint(x86compiler, _vmbase, _vmssp, _vmsbp, rt_ip);
+                    make_checkpoint(x86compiler, _vmbase, _vmssp, _vmsbp, rt_ip, false);
                     break;
                 }
                 case instruct::addr:
