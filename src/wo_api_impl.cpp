6e336000ca8b07363d730a98d2bf4b8a2332d76e
mr cino <583903928@qq.com>
2022-12-20 08:17:45 +0000

    Update wo_api_impl.cpp

--- src/wo_api_impl.cpp
+++ src/wo_api_impl.cpp
@@ -30,8 +30,8 @@
 #define WO_DEBUG_SFX "debug"
 #endif
 
-constexpr wo_integer_t version = WO_VERSION(de, 1, 7, 12);
-constexpr char         version_str[] = WO_VERSION_STR(de, 1, 7, 12) WO_DEBUG_SFX;
+constexpr wo_integer_t version = WO_VERSION(de, 1, 7, 13);
+constexpr char         version_str[] = WO_VERSION_STR(de, 1, 7, 13) WO_DEBUG_SFX;
 
 #undef WO_DEBUG_SFX
 #undef WO_VERSION_STR
