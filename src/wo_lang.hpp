136f6b406cb6e7c01571050ed6b37b2a7bcb01f6
mr_cino <mr_cino@cinogama.net>
2023-08-03 18:03:15 +0800

    * MINOR FIX

--- src/wo_lang.hpp
+++ src/wo_lang.hpp
@@ -375,8 +375,8 @@ namespace wo
             size_t captureindex = (size_t)-1);
         lang_symbol* define_type_in_this_scope(ast::ast_using_type_as* def, ast::ast_type* as_type, ast::ast_decl_attribute* attr);
 
-        bool check_symbol_is_accessable(ast::ast_defines* astdefine, lang_symbol* symbol, lang_scope* current_scope, grammar::ast_base* ast, bool give_error = true);
-        bool check_symbol_is_accessable(lang_symbol* symbol, lang_scope* current_scope, grammar::ast_base* ast, bool give_error = true);
+        bool check_symbol_is_accessable(ast::ast_defines* astdefine, lang_symbol* symbol, lang_scope* current_scope, grammar::ast_base* ast, bool give_error);
+        bool check_symbol_is_accessable(lang_symbol* symbol, lang_scope* current_scope, grammar::ast_base* ast, bool give_error);
 
         lang_symbol* find_symbol_in_this_scope(ast::ast_symbolable_base* var_ident, wo_pstring_t ident_str, int target_type_mask, bool fuzzy_for_err_report);
         lang_symbol* find_type_in_this_scope(ast::ast_type* var_ident);
