2832ae5cadb8ef8f5e00b808d62c577da64c52f5
mr_cino <mr_cino@cinogama.net>
2023-10-04 13:23:47 +0800

    * 补充测试用例

--- src/wo_lang.cpp
+++ src/wo_lang.cpp
@@ -1561,6 +1561,8 @@ namespace wo
         if (a_value_type_check->is_constant)
             return true;
 
+        fully_update_type(a_value_type_check->aim_type, false);
+
         if (a_value_type_check->aim_type->is_pure_pending())
             lang_anylizer->begin_trying_block();
 
@@ -1576,9 +1578,11 @@ namespace wo
             lang_anylizer->end_trying_block();
         }
         else if (a_value_type_check->aim_type->is_pending())
+        {
             lang_anylizer->lang_error(lexer::errorlevel::error, a_value_type_check, WO_ERR_UNKNOWN_TYPE,
                 a_value_type_check->aim_type->get_type_name(false).c_str()
             );
+        }
 
         a_value_type_check->update_constant_value(lang_anylizer);
         return true;
@@ -4799,32 +4803,6 @@ namespace wo
                         }
                     }
                 }
-                if (ast_value_type_check* ast_value_check = dynamic_cast<ast_value_type_check*>(a_value))
-                {
-                    // ready for update..
-                    fully_update_type(ast_value_check->aim_type, false);
-
-                    if (ast_value_check->aim_type->is_pending())
-                    {
-                        lang_anylizer->lang_error(lexer::errorlevel::error, ast_value_check, WO_ERR_UNKNOWN_TYPE
-                            , ast_value_check->aim_type->get_type_name().c_str());
-
-                        auto fuzz_symbol = find_symbol_in_this_scope(ast_value_check->aim_type, ast_value_check->aim_type->type_name, lang_symbol::symbol_type::typing | lang_symbol::symbol_type::type_alias, true);
-                        if (fuzz_symbol)
-                        {
-                            auto fuzz_symbol_full_name = str_to_wstr(get_belong_namespace_path_with_lang_scope(fuzz_symbol));
-                            if (!fuzz_symbol_full_name.empty())
-                                fuzz_symbol_full_name += L"::";
-                            fuzz_symbol_full_name += *fuzz_symbol->name;
-                            lang_anylizer->lang_error(lexer::errorlevel::infom,
-                                fuzz_symbol->type_informatiom,
-                                WO_INFO_IS_THIS_ONE,
-                                fuzz_symbol_full_name.c_str());
-                        }
-                    }
-                    ast_value_check->update_constant_value(lang_anylizer);
-                }
-                //
 
                 WO_TRY_BEGIN;
                 //
