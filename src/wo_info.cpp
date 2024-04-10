21ca81f28bfc43c8d8435c93268ba5e3d7bb7b59
mr_cino <mr_cino@cinogama.net>
2024-04-10 17:08:36 +0800

    Release 1.13.5.3

--- src/wo_info.cpp
+++ src/wo_info.cpp
@@ -17,9 +17,9 @@
 #endif
 
 constexpr wo_integer_t version = 
-    WO_VERSION(1, 13, 5, 2);
+    WO_VERSION(1, 13, 5, 3);
 constexpr char version_str[] = 
-    WO_VERSION_STR(1, 13, 5, 2) WO_DEBUG_SFX;
+    WO_VERSION_STR(1, 13, 5, 3) WO_DEBUG_SFX;
 
 #undef WO_DEBUG_SFX
 #undef WO_VERSION_STR
