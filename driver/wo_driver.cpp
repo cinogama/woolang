4db5e631348047f3630161b97ca5d5dab79d4f2b
mr_cino <mr_cino@cinogama.net>
2025-08-08 11:24:03 +0800

    * TMP: 临时提交，准备一起合到master处理

--- driver/wo_driver.cpp
+++ driver/wo_driver.cpp
@@ -11,29 +11,59 @@ int main(int argc, char** argv)
 
     int ret = 0;
 
-    wo_vm vm1 = wo_create_vm();
-    wo_load_source(vm1, "/vm1.wo", R"(import woo::std; func main(){let mut s = "Helloworld"; return \=std::println(s);;} return main();)");
-    // wo_jit(vm1);
-    wo_value f = wo_run(vm1);
-
-    wo_vm vm2 = wo_create_vm();
-    wo_load_source(vm2, "/vm2.wo", R"(import woo::std; extern func main(f: ()=> void){let mut x = 128; f(); std::println("wtf", x);})");
-    // wo_jit(vm2);
-    wo_run(vm2);
-
-    wo_unref_value vm2main;
-    wo_extern_symb(&vm2main, vm2, "main");
-
-    wo_value s = wo_reserve_stack(vm2, 1, nullptr);
-    wo_set_val(s + 0, f);
-
-    wo_invoke_value(vm2, &vm2main, 1, nullptr, &s);
-    wo_pop_stack(vm2, 1);
-
-    wo_close_vm(vm1);
-    wo_close_vm(vm2);
-
-   /* if (argc >= 2)
+//    wo_vm vm1 = wo_create_vm();
+//    wo_load_source(vm1, "/vm1.wo", R"(
+//        import woo::std; 
+//        let mut s2 = "Helloworld2"; 
+//        func super_depth_foo(f: ()=> string, s: string, n: int)=> void
+//        {
+//            if (n <= 0)
+//                std::println(s, s2, f());
+//            else
+//                super_depth_foo(f, s, n - 1);
+//        }
+//        
+//        func main()
+//        {
+//            let mut s = "Helloworld"; 
+//            return \f: ()=> string = super_depth_foo(f, s, 10000000);;
+//        }
+//        return main();)");
+//    wo_jit(vm1);
+//    wo_value f = wo_run(vm1);
+//
+//    wo_vm vm2 = wo_create_vm();
+//    wo_load_source(vm2, "/vm2.wo", R"(
+//import woo::std; 
+//
+//let mut axab = "String From Vm2";
+//func foo()
+//{
+//    return axab + "Yes~";
+//}
+//
+//extern func main(f: (()=> string)=> void)
+//{
+//    let mut x = 128; 
+//    f(foo); 
+//    std::println("wtf", x, foo());
+//})");
+//    wo_jit(vm2);
+//    wo_run(vm2);
+//
+//    wo_unref_value vm2main;
+//    wo_extern_symb(&vm2main, vm2, "main");
+//
+//    wo_value s = wo_reserve_stack(vm2, 1, nullptr);
+//    wo_set_val(s + 0, f);
+//
+//    wo_invoke_value(vm2, &vm2main, 1, nullptr, &s);
+//
+//    wo_pop_stack(vm2, 1);
+//    wo_close_vm(vm1);
+//    wo_close_vm(vm2);
+//#if 0
+    if (argc >= 2)
     {
         wo_vm vmm = wo_create_vm();
         bool compile_successful_flag = wo_load_file(vmm, argv[1]);
@@ -101,8 +131,8 @@ int main(int argc, char** argv)
         std::cout << "Version: " << wo_version() << std::endl;
         std::cout << "Commit: " << wo_commit_sha() << std::endl;
         std::cout << "Date: " << wo_compile_date() << std::endl;
-    }*/
-
+    }
+// #endif
     wo_finish(nullptr, nullptr);
 
     return ret;
