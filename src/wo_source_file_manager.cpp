ede60c44482964018b5077d76bcbf91e888151b7
mr_cino <mr_cino@cinogama.net>
2026-06-16 18:26:07 +0800

    Adapt source file manager to snprintf-style woort_get_file_loc, bump woort

--- src/wo_source_file_manager.cpp
+++ src/wo_source_file_manager.cpp
@@ -6,7 +6,9 @@
 #include "woort.h"
 
 #include <istream>
+#include <memory_resource>
 #include <streambuf>
+#include <array>
 
 namespace wo
 {
@@ -31,17 +33,25 @@ namespace wo
         std::vector<std::string> search_dir_strings;
         std::vector<const char*> search_dirs;
 
+        std::array<std::byte, 256> buf;
+        std::pmr::monotonic_buffer_resource pool{ buf.data(), buf.size() };
+
         /* Walk the lexer import chain to collect source directories */
         auto finding_lex = lex;
         while (finding_lex.has_value())
         {
             auto* lex_instance = finding_lex.value();
-            char* dir = woort_get_file_loc(
-                lex_instance->get_source_path()->c_str());
-            if (dir != nullptr)
+            const char* src_path = lex_instance->get_source_path()->c_str();
+
+            size_t need = woort_get_file_loc(src_path, nullptr, 0);
+            if (need > 0)
             {
-                search_dir_strings.emplace_back(dir);
-                woort_free(dir);
+                std::pmr::vector<char> dir_buf{ &pool };
+                dir_buf.resize(need + 1);
+
+                (void)woort_get_file_loc(src_path, dir_buf.data(), dir_buf.size());
+
+                search_dir_strings.emplace_back(dir_buf.data());
             }
             finding_lex = lex_instance->get_who_import_me();
         }
